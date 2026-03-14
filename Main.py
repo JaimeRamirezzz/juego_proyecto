ce0f60b75a038673cf4c7169170f31e2c2810a49
Jaime <jaimerzn@gmail.com>
2026-03-14 12:56:45 +0100

    Creacion de la clase enfrentamiento

--- Main.py
+++ Main.py
@@ -2,6 +2,20 @@ import pygame as pg
 pg.init()
 print("prueba")
 window = pg.display.set_mode((1000, 700))
+
+
+
+class enfrentamiento:
+    def __init__(self, ej1, ej2, ej3, ej4, mapa_tablero, enemigos_mapa = list):#ej = entidad jugable
+        self._jugadas = 0
+        self._noEntidades = 4+len(enemigos_mapa) #para tener como de grande es la cola
+        self.tablero = mapa_tablero # porbisional asta el tablero
+        self.ej1 = ej1 #de momento se queda así, pero se puede modificar en el futuro
+        self.orden = Cola() # cola ordenada en funcion de la velocidad
+    def turno(self):
+        pass # self.orden.primer = la entidad a la que le toca atacar
+
+        
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
